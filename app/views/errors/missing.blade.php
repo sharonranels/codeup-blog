3b80277e500470c4207b146b3880e774b5542a79
sharonranels <sharonranels@yahoo.com>
2014-03-25 15:25:54 -0500

    added errors and logging functions

---
+++ app/views/errors/missing.blade.php
@@ -0,0 +1,7 @@
+@extends('/layouts.master')
+
+@section('content')
+
+<h1>Oops, something went wrong!</h1>
+
+@stop
---
+++ app/views/posts/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.master')
+
+@section('content')
+
+
+<div class="blog-post">
+	<form class="form-horizontal" role="form" action="{{{ action('PostsController@store') }}}" method="post">
+	  <div class="form-group">
+	    <label for="title" class="col-sm-2 control-label">Title</label>
+	    <div class="col-sm-10">
+	      <input type="text" class="form-control" id="title" name="title" placeholder="Title" value="{{{ Input::old('title') }}}">
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <label for="body" class="col-sm-2 control-label">Body</label>
+	    <div class="col-sm-10">
+	      <textarea name="body" class="form-control" rows="5">{{{ Input::old('body') }}}</textarea>
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <div class="col-sm-offset-2 col-sm-10">
+	      <button type="submit" class="btn btn-default">Submit</button>
+	    </div>
+	  </div>
+	</form>
+</div>
+
+
+
+
+
+@stop
