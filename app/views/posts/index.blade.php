2006963eaa088acd3224f58d639490da4d0d2141
sharonranels <sharonranels@yahoo.com>
2014-03-27 11:16:00 -0500

    added link from "show" to "create"

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -2,17 +2,20 @@
 
 @section('content')
 
- <div class="blog-header">
-    <h1 class="blog-title">Welcome to my Blog!</h1>
-    <p class="lead blog-description"><em><strong>The Blog site of Sharon Ranels</em></strong></p>
-  </div>
+<div class="blog-header">
+	<h1 class="blog-title">Welcome to my Blog!</h1>
+	<p class="lead blog-description"><em><strong>The Blog site of Sharon Ranels</em></strong></p>
+</div>
 
-	@foreach ($posts as $post)
-	<div>
-		<h2><a href="{{{ action( 'PostsController@show', $post->id) }}}">{{{ $post->title }}}</a></h2>
-		<p>{{{ Str::words($post->body, 8) }}}</p>
-		<p>{{{ $post->created_at }}}</p>
-	</div>
-	@endforeach
+<p><a href="{{{ action( 'PostsController@create') }}}">Create new blog</a></p>
+
+
+@foreach ($posts as $post)
+<div>
+	<h2><a href="{{{ action( 'PostsController@show', $post->id) }}}">{{{ $post->title }}}</a></h2>
+	<p>{{{ Str::words($post->body, 8) }}}</p>
+	<p>{{{ $post->created_at }}}</p>
+</div>
+@endforeach
 
 @stop
