df04aae11e9dd8dddc273e0823b6dbcdb78465a5
sharonranels <sharonranels@yahoo.com>
2014-03-28 11:16:44 -0500

    Date has been formatted and lines added between posted blogs

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -9,7 +9,7 @@ class PostsController extends \BaseController {
 	 */
 	public function index()
 	{
-		$posts = Post::paginate(3);
+		$posts = Post::paginate(4);
 		return View::make('posts.index')->with(array('posts' => $posts));
 	}
 
