3f1a18da0126053ea95385663e9dc8edf8eab8b8
sharonranels <sharonranels@yahoo.com>
2014-03-28 16:01:45 -0500

    final push preparing for web deployment

--- app/config/database.php
+++ app/config/database.php
@@ -52,15 +52,29 @@ return array(
 			'prefix'   => '',
 		),
 
+		// 'mysql' => array(
+		// 	'driver'    => 'mysql',
+		// 	'host'      => 'localhost',
+		// 	'database'  => 'blog_db',
+		// 	'username'  => 'codeup',
+		// 	'password'  => 'codeup',
+		// 	'charset'   => 'utf8',
+		// 	'collation' => 'utf8_unicode_ci',
+		// 	'prefix'    => '',
+
 		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'blog_db',
-			'username'  => 'codeup',
-			'password'  => 'codeup',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
+	    'driver'    => 'mysql',
+	    'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : 'localhost',
+	    'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : 'blog_db',
+	    'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : 'codeup',
+	    'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : 'codeup',
+	    'charset'   => 'utf8',
+	    'collation' => 'utf8_unicode_ci',
+	    'prefix'    => '',
+),
+
+
+
 		),
 
 		'pgsql' => array(
