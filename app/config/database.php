e2fd00ec0487dd09a8b5467e7a820b6c2d8e9d8c
sharonranels <sharonranels@yahoo.com>
2014-03-31 09:08:08 -0500

    removed extra parenthesis

--- app/config/database.php
+++ app/config/database.php
@@ -63,18 +63,14 @@ return array(
 		// 	'prefix'    => '',
 
 		'mysql' => array(
-	    'driver'    => 'mysql',
-	    'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : 'localhost',
-	    'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : 'blog_db',
-	    'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : 'codeup',
-	    'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : 'codeup',
-	    'charset'   => 'utf8',
-	    'collation' => 'utf8_unicode_ci',
-	    'prefix'    => '',
-),
-
-
-
+		    'driver'    => 'mysql',
+		    'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : 'localhost',
+		    'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : 'blog_db',
+		    'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : 'codeup',
+		    'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : 'codeup',
+		    'charset'   => 'utf8',
+		    'collation' => 'utf8_unicode_ci',
+		    'prefix'    => '',
 		),
 
 		'pgsql' => array(
@@ -97,7 +93,7 @@ return array(
 			'prefix'   => '',
 		),
 
-	)
+	),
 
 	/*
 	|--------------------------------------------------------------------------
