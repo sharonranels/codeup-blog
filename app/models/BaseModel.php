7e77e7ecfb8176d200a458cb44c6b1ae5c01b7a4
sharonranels <sharonranels@yahoo.com>
2014-03-31 16:01:19 -0500

    changed timezone get

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -8,13 +8,13 @@ class BaseModel extends Eloquent {
 	public function getCreatedAtAttribute($value)
 	{
     	$utc = Carbon::createFromFormat($this->getDateFormat(), $value);
-    	return $utc->setTimezone('America/Chicago');
+    	return $utc->setTimezone(Config::get('app.localTimezone'));
 	}
 
 	public function getUpdatedAtAttribute($value)
 	{
     	$utc = Carbon::createFromFormat($this->getDateFormat(), $value);
-    	return $utc->setTimezone('America/Chicago');
+    	return $utc->setTimezone(Config::get('app.localTimezone'));
 	}	
 
 
