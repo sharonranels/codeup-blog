5bf82a3993621cdbe3a93f8cde6f3dead83395d9
sharonranels <sharonranels@yahoo.com>
2014-04-01 14:35:57 -0500

    added a login page

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -15,6 +15,7 @@
 
      <link rel="stylesheet" href="/css/resume-portfolio-styling.css">
 
+     <link href="/css/signin.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
