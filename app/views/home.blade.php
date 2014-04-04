db5eac03f347d90f555a5988fb17a38262d4dd74
sharonranels <sharonranels@yahoo.com>
2014-04-04 09:08:30 -0500

    fixed formatting in text in home page and added navbar background to other pages

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -10,18 +10,17 @@
 
   <div class="row home">
     
-    <div class="col-sm-8"><img ALIGN="Left" src="/img/Sharon.jpg" alt="Sharon Ranels" class="headshot-thumbnail">
+    <div class="col-sm-5"><img ALIGN="Left" src="/img/Sharon.jpg" alt="Sharon Ranels" class="headshot-thumbnail"></div>
 
 <!-- CONTACT INFO -->
         
-        <div class="col-sm-4 home-contact">
-            <div>(832) 298-4240</div>
-            <div><a class="home-contact" href="mailto:sharonranels@yahoo.com">sharonranels@yahoo.com</a></div>
-            <div><a href="http://www.linkedin.com/in/sharonranels/" target="_blank"><img src="/img/linkedinlogo2.jpeg" alt="Linked In" class="contact-height">www.linkedin.com/in/sharonranels</a></div>
-            <div><a href="https://github.com/sharonranels" target="_blank"><img src="/img/github.jpeg" alt="Git Hub" class="contact-height">https://github.com/sharonranels</a></div>
+        <div class="col-sm-5 home-contact">
+            <div id="hover-format"><a href="tel:1-832-298-4240">(832) 298-4240</a></div>
+            <div><a href="mailto:sharonranels@yahoo.com">sharonranels@yahoo.com</a></div>
+            <div><a href="https://github.com/sharonranels" target="_blank"><img src="/img/github.jpeg" alt="Git Hub" class="contact-height">&nbsp;&nbsp;github.com/sharonranels</a></div>
+            <div><a href="http://www.linkedin.com/in/sharonranels/" target="_blank"><img src="/img/linkedinlogo2.jpeg" alt="Linked In" class="contact-height">&nbsp;&nbsp;linkedin.com/in/sharonranels</a></div>
         </div>  
     </div>
-  </div>
 
 
 @stop
