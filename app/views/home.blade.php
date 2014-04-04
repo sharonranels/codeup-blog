d7b391b5a3f15a08e4d1b9eef3d233403a325b4e
sharonranels <sharonranels@yahoo.com>
2014-04-03 20:56:14 -0500

    home page working with a couple of issues
    issues - git and linkedin icons not lining up with address, addresses
    are blue instead of white, need space between contact lines

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -8,22 +8,18 @@
 
 <!-- Add columns for name and info -->
 
-  <div class="row">
-    <div class="col-md-9"><img src="/img/Sharon.jpg" alt="Sharon Ranels" class="headshot-thumbnail"></div>
-    <div class="col-md-9" id="div-height"><em><strong class="namefont">Sharon Ranels</strong></em>
-    Web Developer</div>
+  <div class="row home">
+    
+    <div class="col-sm-9"><img ALIGN="Left" src="/img/Sharon.jpg" alt="Sharon Ranels" class="headshot-thumbnail">
 
 <!-- CONTACT INFO -->
-    
-    <div class="col-md-3"><strong>CONTACT INFORMATION</strong></div>
-    
-    <div class="col-md-4">San Antonio TX<br>
-    (832) 298-4240<br>
-    <a href="mailto:sharonranels@yahoo.com">sharonranels@yahoo.com</a><br></div>
-    
-    <div class="col-md-4"><a href="http://www.linkedin.com/in/sharonranels/" target="_blank"><img src="/img/linkedinlogo2.jpeg" alt="Linked In" class="contact-height"></a><br>
-    <a href="https://github.com/sharonranels" target="_blank"><img src="/img/github.jpeg" alt="Git Hub" class="contact-height"></a></div>  
-  
+        
+        <div class="col-sm-3 home-contact">(832) 298-4240<br>
+            <a class="home-contact" href="mailto:sharonranels@yahoo.com">sharonranels@yahoo.com</a><br>
+            <a href="http://www.linkedin.com/in/sharonranels/" target="_blank"><img src="/img/linkedinlogo2.jpeg" alt="Linked In" class="contact-height">www.linkedin.com/in/sharonranels</a><br>
+            <a href="https://github.com/sharonranels" target="_blank"><img src="/img/github.jpeg" alt="Git Hub" class="contact-height">https://github.com/sharonranels</a>
+        </div>  
+    </div>
   </div>
 
 
