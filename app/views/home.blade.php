6a79e14d5aa5f5d3a24b5cf1906803e3a4248ecc
sharonranels <sharonranels@yahoo.com>
2014-04-03 21:48:08 -0500

    more adjustments trying to fix formatting

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -14,10 +14,11 @@
 
 <!-- CONTACT INFO -->
         
-        <div class="col-sm-3 home-contact">(832) 298-4240<br>
+        <div class="col-sm-3 home-contact">
+            (832) 298-4240<br>
             <a class="home-contact" href="mailto:sharonranels@yahoo.com">sharonranels@yahoo.com</a><br>
-            <a href="http://www.linkedin.com/in/sharonranels/" target="_blank"><img src="/img/linkedinlogo2.jpeg" alt="Linked In" class="contact-height">www.linkedin.com/in/sharonranels</a><br>
-            <a href="https://github.com/sharonranels" target="_blank"><img src="/img/github.jpeg" alt="Git Hub" class="contact-height">https://github.com/sharonranels</a>
+            <a href="http://www.linkedin.com/in/sharonranels/" target="_blank">www.linkedin.com/in/sharonranels<img src="/img/linkedinlogo2.jpeg" alt="Linked In" class="contact-height"></a><br>
+            <a href="https://github.com/sharonranels" target="_blank">https://github.com/sharonranels<img src="/img/github.jpeg" alt="Git Hub" class="contact-height"></a>
         </div>  
     </div>
   </div>
